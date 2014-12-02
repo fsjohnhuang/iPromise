e22daa84bf73e9b10b44bbb2edc57d49daa798ac
fsjohnhuang@hotmail.com <fsjohnhuang@hotmail.com>
2014-12-02 15:32:56 +0800

    make some notes

--- theory/PromisesA/promise-6.0.0-browser.js
+++ theory/PromisesA/promise-6.0.0-browser.js
@@ -6,21 +6,44 @@
 	var Promise = exports.Promise = function (fn) {
       if (typeof this !== "object") throw new TypeError("Promises must be constructed via new");
       if (typeof fn !== "function") throw new TypeError("not a function");
-      var state = null;
-      var value = null;
-      var deferreds = [];
+      var state = null; // 状态，null：pending，true：fulfilled，false：rejected
+      var value = null; // 当前promise的状态事件处理函数（onFulfilled或onRejected）的入参
+      var deferreds = []; // 当前promise的状态事件处理函数和promise链表中下一个promise的状态转换发起函数
       var self = this;
+      // 唯一的公开方法
       this.then = function(onFulfilled, onRejected) {
+        // 构造新的promise实例并返回，从而形成链式操作
         return new self.constructor(function(resolve, reject) {
-          handle(new Handler(onFulfilled, onRejected, resolve, reject));
+          var handler = new Handler(onFulfilled, onRejected, resolve, reject);
+          /* 
+          注意：这里利用了闭包特性，此处的handle并不是新Promise的handle函数，而是this.then所属promise的handle函数。
+              因此handler将被添加到this.then所属promise的deffereds数组中。
+              而onFulfilled和onRejected自然成为了this.then所属promise的状态转换事件处理函数，
+              而resolve和reject依旧是新promise实例的状态转换触发函数。
+          */
+          handle(handler);
         });
       };
+      // 保存和执行deferreds数组中的元素
       function handle(deferred) {
         if (state === null) {
           deferreds.push(deferred);
           return;
         }
+        // asap的作用为将入参的操作压入event loop队列中
         asap(function() {
+          /* 下面是简化后的实现方式，个人感觉会直观一些
+          var cb = deferred[state ? 'onFulfilled' : 'onRejected'];
+          var deferredAction = 'resolve', ret;
+          try{
+            ret = cb ? cb(value) : value;
+          }
+          catch (e){
+            ret = e;
+            deferredAction = 'reject';
+          }
+          deferred[deferredAction].call(deferred, ret);*/
+
           var cb = state ? deferred.onFulfilled : deferred.onRejected;
           if (cb === null) {
             (state ? deferred.resolve : deferred.reject)(value);
@@ -28,20 +51,25 @@
           }
           var ret;
           try {
+            // 执行当前promise的状态转换事件处理函数
             ret = cb(value);
           } catch (e) {
+            // 修改promise链表中下一个promise对象的状态为rejected
             deferred.reject(e);
             return;
           }
+          // 修改promise链表中下一个promise对象的状态为fulfilled
           deferred.resolve(ret);
         });
       }
+      // promise的状态转换发起函数，触发promise的状态从pending->fulfilled
       function resolve(newValue) {
         try {
           if (newValue === self) throw new TypeError("A promise cannot be resolved with itself.");
           if (newValue && (typeof newValue === "object" || typeof newValue === "function")) {
             var then = newValue.then;
             if (typeof then === "function") {
+              // 将控制权移交thenable和promise对象，由它们来设置当前pormise的状态和状态转换事件处理函数的实参
               doResolve(then.bind(newValue), resolve, reject);
               return;
             }
@@ -53,24 +81,30 @@
           reject(e);
         }
       }
+      // promise的状态转换发起函数，触发promise的状态从pending->rejected
       function reject(newValue) {
         state = false;
         value = newValue;
         finale();
       }
+      // 向链表的下一个promise移动
       function finale() {
         for (var i = 0, len = deferreds.length; i < len; i++) handle(deferreds[i]);
         deferreds = null;
       }
+      // 执行构造函数的工厂方法，由工厂方法触发promise的状态转换
       doResolve(fn, resolve, reject);
     }
+    // 构造promise的链表逻辑结构
     function Handler(onFulfilled, onRejected, resolve, reject) {
       this.onFulfilled = typeof onFulfilled === "function" ? onFulfilled : null;
       this.onRejected = typeof onRejected === "function" ? onRejected : null;
       this.resolve = resolve;
       this.reject = reject;
     }
+    // 对状态转换事件处理函数进行封装后，再传给执行函数
     function doResolve(fn, onFulfilled, onRejected) {
+      // done作为开关以防止fn内同时调用resolve和reject方法
       var done = false;
       try {
         fn(function(value) {
@@ -89,7 +123,9 @@
       }
     }
 
-     function ValuePromise(value) {
+    // 将非thenable对象构造为thenable对象
+    // 其then方法则返回一个真正的Promise对象
+    function ValuePromise(value) {
       this.then = function(onFulfilled) {
         if (typeof onFulfilled !== "function") return this;
         return new Promise(function(resolve, reject) {
@@ -103,6 +139,13 @@
         });
       };
     }
+    /*
+     也可以将非thenable对象构造为Promise对象
+    function ValuePromise(value){
+      return new Promise(function(resolve){
+        resolve(value);
+      });
+    }*/
     ValuePromise.prototype = Promise.prototype;
     var TRUE = new ValuePromise(true);
     var FALSE = new ValuePromise(false);
@@ -143,12 +186,14 @@
               var then = val.then;
               if (typeof then === "function") {
                 then.call(val, function(val) {
+                  // 对于thenable和promise对象则订阅onFulfilled事件获取处理结果值
                   res(i, val);
                 }, reject);
                 return;
               }
             }
             args[i] = val;
+            // 检测是否所有入参都已返回值
             if (--remaining === 0) {
               resolve(args);
             }
@@ -169,10 +214,30 @@
     Promise.race = function(values) {
       return new Promise(function(resolve, reject) {
         values.forEach(function(value) {
+          // 将数组元素转换为promise对象
+          // 由resolve函数内部的标识位done来保证onFulfilled函数仅执行过一次
           Promise.resolve(value).then(resolve, reject);
         });
       });
     };
+
+    /* 也可以通过这样形式实现
+    Promise.race = function(values){
+      return new Promise(function(resolve, reject){
+        var over = 0;
+        for (var i = 0, len = values.length; i < len && !over; ++i){
+          var val = values[i];
+          if (val && typeof val.then === 'function'){
+            val.then(function(res){
+              !over++ && resolve(res);
+            }, reject);
+          }
+          else{
+            !over++ && resolve(val);
+          }
+        }
+      });
+    };*/
     Promise.prototype["catch"] = function(onRejected) {
       return this.then(null, onRejected);
     };
