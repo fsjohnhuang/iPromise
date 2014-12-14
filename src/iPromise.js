84b774dcfd358385b9d1446b194bdc567c84661f
fsjohnhuang@hotmail.com <fsjohnhuang@hotmail.com>
2014-12-15 00:06:39 +0800

    update v0.0.2

--- src/iPromise.js
+++ src/iPromise.js
@@ -1,85 +1,135 @@
 /*!
  * An implementation for Promises/A+
  * @author fsjohnhuang
- * @version v0.0.1
+ * @version v0.0.2
  */
-;(function(exports){
-	if (!Function.prototype.bind)
-		Function.prototype.bind = function(self){
-			var fn = this;
-			return function(){
-				fn.apply(self, arguments);
-			};
-		};
+;(function(exports, undefined){
+	var Version = '0.0.2'
 
-	var handle = function(_){
-		var state = _.state, 
-			data = _.data, 
-			thenables = _.thenables, 
-			handlerHame = state === 1 ? 'onFulfilled' : 'onRejected',
-			data4NoHandler = state === 1 ? data : undefined,
-			thenable;
-		while(thenable = thenables.shift()){
-			try{
-				var handler = thenable[handlerHame];
-				if (handler){
-					var ret = handler(data);
-					if (ret && typeof ret.then === 'function'){
-						ret.then(thenable.promise.resolve.bind(thenable.promise)
-							,thenable.promise.reject.bind(thenable.promise));
-					}
-					else{
-						thenable.promise.resolve(ret);	
-					}
-				}	
-				else{
-					thenable.promise.resolve(data4NoHandler);
+	var iPromise = exports.iPromise = function(mixin){
+		var state = {
+			dirty: false,
+			status: 'pending',
+			fulfilled: function(v){
+				state.status = 'fulfilled'
+				return v
+			},
+			rejected: function(r){
+				state.status = 'rejected'
+				return r
+			},
+			progress: function(v){return v},
+			finally: function(){}
+		}
+
+		var deferred = {
+			then: function(fulfilledFn, rejectedFn, progressFn, finallyFn){
+				return post(state, arguments)
+			},
+			catch: function(rejectedFn){
+				return post(state, [0].concat(toArray(arguments)))
+			},
+			progress: function(progressFn){
+				return post(state, [0, 0].concat(toArray(arguments)))
+			},
+			finally: function(finallyFn){
+				return post(state, [0, 0, 0].concat(toArray(arguments)))
+			}
+		}
+
+		for (var i = 0, len = statusMethodTuple.length; i < len; ++i)
+			deferred[statusMethodTuple[i][1]] = function(i){
+				return function (){
+					if (i < 2 && state.status !== 'pending') return
+
+					if (state.dirty)
+						fire(state, i, arguments)
+					else
+						setImmediate(fire, state, i, arguments)
 				}
+			}(i)
+
+		return deferred
+	}
+
+	var post = function(state, fns){
+		state.dirty = true
+		var i = -1
+		'fulfilled rejected progress finally'.replace(/\w+/g, function(method){
+			var fn = fns[++i]
+			if (typeof fn !== 'function') return
+
+			state[method] = i >= 2 ? fn : function(){
+				var val
+				try{
+					val = fn.apply(this, arguments)
+					state.status = 'fulfilled'
+				}
+				catch(e){
+					val = e
+					state.status = 'rejected'
+				}
+				return val
+			}
+		})
+
+		state.next = iPromise()
+		return extend({}, state.next, 'then catch progress finally')
+	}
+
+	var fire = function(state, idxOfTuple, args){
+		var val = state[statusMethodTuple[idxOfTuple][0]].apply(this, args)
+		if (val && typeof val.then === 'function' && state.next)
+			return val.then(state.next.resolve, state.next.reject, state.next.notify)
+
+		var method = idxOfTuple === 2 ? statusMethodTuple[idxOfTuple][1] : function(){
+			try{
+				state['finally']()
 			}
 			catch(e){
-				thenable.promise.reject(e);
+				state.status = 'rejected'
+				val = e
 			}
+			return statusMethodTuple.find(state.status)[1]
+		}()
+		state.next && state.next[method](val)
+	}
+
+	var toArray = function(arrayLike){
+		try{
+			return [].slice.call(arrayLike)
 		}
-	};
-	var complete = function(state, _, data){
-		if (_.state !== 0) return;
-		_.state = state;
-		_.data = data;
-		handle(_);
-	};
-
-	var iPromise = exports.iPromise = function(fn){
-		if (!(this instanceof iPromise))
-			return new iPromise(fn);
-
-		var _ = this._ = {};
-		_.state = 0; // 0:pending, 1:fulfilled, 2:rejected
-		_.thenables = [];
-
-		fn && fn(this.resolve.bind(this), this.reject.bind(this));
-	};	
-	iPromise.prototype.then = function(fulfilledHandler, rejectedHandler){
-		var _ = this._;
-		var promise = new iPromise();
-		var thenable = {
-			onFulfilled: fulfilledHandler,
-			onRejected: rejectedHandler,
-			promise: promise
-		};
-		_.thenables.push(thenable);
-
-		if (_.state !== 0){
-			window[window.setImmediate ? 'setImmediate' : 'setTimeout'].call(window, function(){
-				handle(_);
-			}, 0);	
+		catch(e){
+			var array = []
+			for (var i = 0, len = arrayLike.length; i < len; ++i)
+				array[i] = arrayLike[i]
+			return array
 		}
+	}
+	var extend = function(target, other, props){
+		for (var i in other){
+			if (typeof props !== 'string' || new RegExp(i).test(props))
+				target[i] = other[i]
+		}
+		return target
+	}
+	var setImmediate = window.setImmediate || function(fn){
+		var args = [].slice.call(arguments, 1)	
+		setTimeout(function(){
+			fn.apply(this, args)
+		}, 0)
+	}
+	var statusMethodTuple = [
+		['fulfilled', 'resolve'],
+		['rejected', 'reject'],
+		['progress', 'notify']
+	] 
+	statusMethodTuple.find = function(key){
+		var tokens = (this + '').split(',')
+		for (var i = 0, len = tokens.length; i < len; ++i)
+			if (tokens[i] === key) break	
 
-		return promise;
-	};
-	iPromise.prototype.resolve = function(data){
-		complete(1, this._, data);
-	};
-	iPromise.prototype.reject = function(data){
-		complete(2, this._, data);
-	};
-}(window));
+		return this[i/2]
+	}
+
+}(window, void 0));
